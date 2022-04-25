2d623b979528461e4b4a045153c6f3bcb36aa6eb
petit.milou@live.fr <petit.milou@live.fr>
2022-04-25 15:31:04 +0200

    test api competences

---
+++ api/serveur_cv_competences/src/main/java/ca/uqac/api/entity/CVCompetence.java
@@ -0,0 +1,14 @@
+package ca.uqac.api.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CVCompetence {
+
+    private String id;
+    private List<String> competence
+}
